1717d810a8d2cdd5c2a113bb9c85b79cccb673ed
shilpa-026 <72601194+shilpa-026@users.noreply.github.com>
2021-05-31 00:01:55 +0530

    Add files via upload

---
+++ angular_practice_from_Angular.io/src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Input} from '@angular/core'
+import { Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product: Product|undefined;
+  @Output() notify = new EventEmitter();
+  happy_str=""
+  constructor() { }
+
+  ngOnInit() {
+  }
+  happy(){
+    this.happy_str="happy happy";
+  }
+
+}
